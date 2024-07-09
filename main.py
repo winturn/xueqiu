37b5c66132aa580db14824ef1915fbffe9d994a5
winturn <win_turn@163.com>
2024-07-09 11:21:28 +0800

    忽略意料中的报错

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from unility import parsedata
 import time
 import datetime
 import os
+import warnings
+from bs4 import MarkupResemblesLocatorWarning
 
 
 def get_desktop_path():
@@ -12,9 +14,10 @@ def get_desktop_path():
 
 
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
     parse_data = parsedata.ParseData()
     net_my = network.Network('1247347556')  # 用户id，例如大道的id是1247347556
-    for i in range(1, 2):  # range的范围是 https://xueqiu.com/u/1247347556 页面的页数
+    for i in range(1, 10):  # range的范围是 https://xueqiu.com/u/1247347556 页面的页数
         print(i)
         net_my.get_page_url(i)
         res_json = net_my.res.json()
@@ -22,6 +25,7 @@ if __name__ == '__main__':
         time.sleep(2)
     desktop_path = get_desktop_path()
     parse_data.save_to_file(
+        # 文件保存到桌面，文件名为当前日期
         os.path.join(desktop_path, r'{}.xlsx'.format(datetime.datetime.now().strftime('%Y%m%d')))
     )
 
